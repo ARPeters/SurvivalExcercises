5d0c7e24b7e87119cd7ccd953378e261a9f732fd
Andrew Peters <andrew.r.peters.42@gmail.com>
2014-12-16 15:15:14 -0600

    Cleaned up and reorganized Poisson code

--- Andrew/Tables-CH4 and 5-KK.R
+++ Andrew/Tables-CH4 and 5-KK.R
@@ -10,15 +10,19 @@ dsVets <- read.dta("http://web1.sph.emory.edu/dkleinb/allDatasets/surv2datasets/
 names(dsVets)<-c("tx", "Large", "Adeno", "Small", "Squamous", "survt", "perf", "DisDur", "age", "priortx", "status")
 
 #Fitting full COX PH Model
-coxphPractice1<-coxph(Surv(dsVets$survt, dsVets$status==1)~dsVets$tx+dsVets$Large+dsVets$Adeno+dsVets$Small+dsVets$perf+dsVets$DisDur+dsVets$age+dsVets$priortx, ties="breslow")
+coxphPractice1<-coxph(Surv(dsVets$survt, dsVets$status==1)~tx+Large+Adeno+Small+perf+DisDur+age+priortx, ties="breslow", data=dsVets)
 coxphPractice1
 
-#Testing PH Assumption for each predictor
+#Fitting a Poisson regression model
+glm_pr_Practice1<-glm(status~tx+Large+Adeno+Small+perf+DisDur+age+priortx +offset(log(survt)), family="poisson", data=dsVets)
+glm_pr_Practice1
 
+#Testing PH Assumption for each predictor
 cox.zph(coxphPractice1)
 
 dsVetsEvents<-dsVets[dsVets$status==1,]
-coxphPractice1Events<-coxph(Surv(dsVetsEvents$survt, dsVetsEvents$status==1)~dsVetsEvents$tx+dsVetsEvents$Large+dsVetsEvents$Adeno+dsVetsEvents$Small+dsVetsEvents$perf+dsVetsEvents$DisDur+dsVetsEvents$age+dsVetsEvents$priortx, ties="breslow")
+coxphPractice1Events<-coxph(Surv(dsVetsEvents$survt, dsVetsEvents$status==1)~tx+Large+Adeno+Small+perf+DisDur+age+priortx, 
+                            ties="breslow", data=dsVetsEvents)
 
 cox.zph(coxphPractice1Events, transform="rank")
 
@@ -30,47 +34,80 @@ cox.zph(coxphPractice1Events, transform="rank")
 dsAddicts<- read.dta("http://web1.sph.emory.edu/dkleinb/allDatasets/surv2datasets/addicts.dta")
 
 #Fitting Full Cox PH Model
-coxphTest1<-coxph(Surv(dsAddicts$survt, dsAddicts$status==1)~dsAddicts$clinic+dsAddicts$prison+dsAddicts$dose, ties="breslow")
+coxphTest1<-coxph(Surv(dsAddicts$survt, dsAddicts$status==1)~clinic+prison+dose, ties="breslow", data=dsAddicts)
 coxphTest1
 
 cox.zph(coxphTest1)
 
 #Testing PH Assumption for each predictor
 dsAddictsEvents<-dsAddicts[dsAddicts$status==1,]
-coxphTest1Events<-coxph(Surv(dsAddictsEvents$survt, dsAddictsEvents$status==1)~dsAddictsEvents$clinic+dsAddictsEvents$prison+dsAddictsEvents$dose, ties="breslow")
+coxphTest1Events<-coxph(Surv(dsAddictsEvents$survt, dsAddictsEvents$status==1)~clinic+prison+dose, ties="breslow", data=dsAddictsEvents)
 cox.zph(coxphTest1Events, transform="rank")
 
+#Fitting a Poisson regression model
+glm_pr_Test1<-glm(status~clinic+prison+dose+offset(log(survt)), family="poisson", data=dsAddicts)
+glm_pr_Test1
+
 #Fitting a cox ph model stratifying on clinic
-coxphAddictsClinic<-coxph(Surv(dsAddicts$survt, dsAddicts$status==1)~dsAddicts$prison+dsAddicts$dose+strata(dsAddicts$clinic), ties="breslow")
+coxphAddictsClinic<-coxph(Surv(dsAddicts$survt, dsAddicts$status==1)~prison+dose+strata(clinic), ties="breslow", data=dsAddicts)
 coxphAddictsClinic
+
+#Fitting a Poisson regression model
+glm_pr_Clinic<-glm(status~prison*strata(clinic)+dose*strata(clinic)+offset(log(survt)), family="poisson", data=dsAddicts)
+glm_pr_Clinic
 
 
 ################################
 #CHAPTER 5: Practice
 ################################
 
-coxphPractice1Full<-coxph(Surv(dsVets$survt, dsVets$status)~dsVets$tx+dsVets$Large+dsVets$Adeno+dsVets$Small+dsVets$perf+dsVets$DisDur+dsVets$age+dsVets$priortx, ties="breslow")
+coxphPractice1Full<-coxph(Surv(dsVets$survt, dsVets$status==1)~tx+Large+Adeno+Small+perf+DisDur+age+priortx, ties="breslow", data=dsVets)
 coxphPractice1Full
 
-coxphPractice1Reduced<-coxph(Surv(dsVets$survt, dsVets$status)~dsVets$tx+dsVets$Small+dsVets$perf+dsVets$DisDur+dsVets$age+dsVets$priortx, ties="breslow")
+glm_pr_Practice1Full<-glm(status~tx+Large+Adeno+Small+perf+DisDur+age+priortx +offset(log(survt)), family="poisson", data=dsVets)
+glm_pr_Practice1Full
+
+coxphPractice1Reduced<-coxph(Surv(dsVets$survt, dsVets$status==1)~tx+Small+perf+DisDur+age+priortx, ties="breslow", data=dsVets)
 coxphPractice1Reduced
+
+glm_pr_Practice1Reduced<-glm(status~tx+Small+perf+DisDur+age+priortx+offset(log(survt)), family="poisson", data=dsVets)
+glm_pr_Practice1Reduced
+
 
 #Question3
 perfStatus<-ifelse(dsVets$perf>59, 1, 0)
 dsVets<-cbind(dsVets, perfStatus)
 
-coxphVetsStrata3<-coxph(Surv(dsVets$survt, dsVets$status)~dsVets$tx+dsVets$DisDur+dsVets$age+dsVets$priortx +strata(dsVets$Small, dsVets$perfStatus), ties="breslow")
+coxphVetsStrata3<-coxph(Surv(dsVets$survt, dsVets$status)~tx+DisDur+age+priortx +strata(Small, perfStatus), ties="breslow", data=dsVets)
 coxphVetsStrata3
 
-#Question 10:
+#Fitting a Poisson regression model
+# Neither of these is a great fit; not sure how to reconcile this with a stratified cox model. 
+glm_pr_Strata3<-glm(status~tx+DisDur+age+priortx+offset(log(survt))+strata(Small, perfStatus), family="poisson", data=dsVets)
+glm_pr_Strata3
 
-coxphVetsStrata10<-coxph(Surv(dsVets$survt, dsVets$status)~dsVets$tx+dsVets$DisDur+dsVets$age+dsVets$priortx+
-                           dsVets$tx*strata(dsVets$Small, dsVets$perfStatus)+dsVets$DisDur*strata(dsVets$Small, dsVets$perfStatus)+dsVets$age*strata(dsVets$Small, dsVets$perfStatus)+dsVets$priortx*strata(dsVets$Small, dsVets$perfStatus)
-                         , ties="breslow")
+glm_pr_Strata3<-glm(status~tx*strata(Small, perfStatus)+DisDur*strata(Small, perfStatus)+age*strata(Small, perfStatus)+
+                      priortx*strata(Small, perfStatus)+offset(log(survt))+strata(Small, perfStatus), family="poisson", data=dsVets)
+glm_pr_Strata3
+
+#Question 10:
+coxphVetsStrata10<-coxph(Surv(dsVets$survt, dsVets$status)~tx+DisDur+age+priortx+
+                           tx*strata(Small, perfStatus)+DisDur*strata(Small, perfStatus)+
+                           age*strata(Small, perfStatus)+priortx*strata(Small, perfStatus)
+                         , ties="breslow", data=dsVets)
+
+glm_pr_Strata10<-glm(status~tx+DisDur+age+priortx+offset(log(survt))+tx*strata(Small, perfStatus)+DisDur*strata(Small, perfStatus)
+                     +age*strata(Small, perfStatus)+priortx*strata(Small, perfStatus),
+                     family="poisson", data=dsVets)
 
 coxphVetsStrata10<-coxph(Surv(dsVets$survt, dsVets$status)~
-                           dsVets$tx*strata(dsVets$Small, dsVets$perfStatus)+dsVets$DisDur*strata(dsVets$Small, dsVets$perfStatus)+dsVets$age*strata(dsVets$Small, dsVets$perfStatus)+dsVets$priortx*strata(dsVets$Small, dsVets$perfStatus)
-                         , ties="breslow")
+                           tx*strata(Small, perfStatus)+DisDur*strata(Small, perfStatus)+age*strata(Small, perfStatus)+
+                           priortx*strata(Small, perfStatus)
+                         , ties="breslow", data=dsVets)
+
+glm_pr_Strata10<-glm(status~offset(log(survt))+tx*strata(Small, perfStatus)+DisDur*strata(Small, perfStatus)
+                     +age*strata(Small, perfStatus)+priortx*strata(Small, perfStatus),
+                     family="poisson", data=dsVets)
 
 summary(coxphVetsStrata10)
 
@@ -81,21 +118,27 @@ plot(coxphVetsStrata10)
 ################################
 
 #Question 1:
-coxphTest1<-coxph(Surv(dsAddicts$survt, dsAddicts$status)~dsAddicts$clinic+dsAddicts$prison+dsAddicts$dose, ties="breslow")
+coxphTest1<-coxph(Surv(dsAddicts$survt, dsAddicts$status)~clinic+prison+dose, ties="breslow", data=dsAddicts)
 coxphTest1
 
 cox.zph(coxphTest1, transform="rank")
 
+glm_pr_Test1<-glm(status~clinic+prison+dose+offset(log(survt)), family="poisson", data=dsAddicts)
+glm_pr_Test1
+
 #Question 2:
-coxphTest2<-coxph(Surv(dsAddicts$survt, dsAddicts$status)~dsAddicts$prison+dsAddicts$dose + strata(dsAddicts$clinic), ties="breslow")
+coxphTest2<-coxph(Surv(dsAddicts$survt, dsAddicts$status)~prison+dose + strata(clinic), ties="breslow", data=dsAddicts)
 coxphTest2
 
 cox.zph(coxphTest2, transform="rank")
 
-#Question 5
-coxphTest5<-coxph(Surv(dsAddicts$survt, dsAddicts$status)~dsAddicts$prison+dsAddicts$dose
-                  +dsAddicts$prison*strata(dsAddicts$clinic)+dsAddicts$dose*strata(dsAddicts$clinic), ties="breslow")
+glm_pr_Test2<-glm(status~ prison + dose + offset(log(survt)) + strata(clinic), family="poisson", data=dsAddicts)
+glm_pr_Test2
 
-aov(coxphTest2, coxphTest5)
-?qchisq()
-pchisq(1.88, df=2, lower.tail=FALSE)
+#Question 5
+coxphTest5<-coxph(Surv(dsAddicts$survt, dsAddicts$status)~prison+dose
+                  +prison*strata(clinic)+dose*strata(clinic), ties="breslow", data=dsAddicts)
+
+glm_pr_Test5<-glm(status~prison +dose + offset(log(survt)) + prison*strata(clinic) + dose*strata(clinic),
+                  family="poisson", data=dsAddicts)
+glm_pr_Test5
